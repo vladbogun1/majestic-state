90b448b273425bbf277d84048f2051729536cd17
V_lastelin <vladbogun@gmail.com>
2026-03-20 02:23:20 +0400

    Fix JDA button import in HR listener

--- src/main/java/com/majesticstate/bot/service/HrCommandListener.java
+++ src/main/java/com/majesticstate/bot/service/HrCommandListener.java
@@ -29,7 +29,7 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.modals.Modal;
 import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 import net.dv8tion.jda.api.components.label.Label;
